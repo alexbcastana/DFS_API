cef9049e70e392db516988454c633576b6924634
alexbcastana <alexbcastana@gmail.com>
2018-01-28 09:38:26 -0800

    Added Base Web API

---
+++ WebApplication2/Models/Player.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.Models
+{
+    public class Player
+    {
+        private int Id { get; set; }
+        private string FName { get; set; }
+        private string LName { get; set; }
+        private int Salary { get; set;}
+    }
+}
